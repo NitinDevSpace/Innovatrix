23b24706fc9e1701fef873c837d2831b745e4b91
Nitin <nitindevspace@gmail.com>
2025-10-03 14:12:17 +0530

    Routing Done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,27 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import About from "./pages/About";
+import KanbanBoard from "./pages/KanbanBoard";
+import Organizers from "./pages/Organizers";
+import Builders from "./pages/Builders";
 
 function App() {
 	return (
 		<>
-			<div className="flex justify-center items-center bg-red-700">
-				<h1 className="flex justify-center items-center text-b">Hello</h1>
-			</div>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/organizers" element={<Organizers />} />
+				<Route path="/builders" element={<Builders />} />
+				<Route path="/kanban" element={<KanbanBoard />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+			<Footer />
 		</>
 	);
 }
