a5194cfec69accf221927e4a480fd87b9dd1bd84
Nitin <nitindevspace@gmail.com>
2025-10-03 12:13:32 +0530

    Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='flex justify-center items-center text-2xl text-red'>Hello gjhgjh</h1>
+    </>
+  )
+}
+
+export default App
