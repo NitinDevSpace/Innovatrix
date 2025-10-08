8cd40a0d64b938baea09a029f54424614e2c5a28
Nitin <nitindevspace@gmail.com>
2025-10-08 13:08:17 +0530

    Removed Server

--- server/src/main/java/com/example/hackon_backend/repository/BuilderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.hackon_backend.repository;
-
-import com.example.hackon_backend.entity.Builder;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BuilderRepository extends JpaRepository<Builder, String> {
-}
--- server/src/main/java/com/example/hackon_backend/repository/OrganizerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.hackon_backend.repository;
-
-import com.example.hackon_backend.entity.Organizer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrganizerRepository extends JpaRepository<Organizer, String> {
-}
