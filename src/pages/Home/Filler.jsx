c1ab27d73f3f7fb964e0bec6bddad3ecec4c427a
Nitin <nitindevspace@gmail.com>
2025-10-13 13:42:44 +0530

    Filler section filled

--- src/pages/Home/Filler.jsx
+++ src/pages/Home/Filler.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
 
 function Filler() {
-  return (
-      <div className='bg-primary mx-auto'>
-          <div className='h-[60vh]'>Filler</div>
-    </div>
-  )
+	return (
+		<div className="flex justify-around items-center bg-primary">
+			<div>
+				<h1 className="text-white text-5xl font-grotesk">Build.</h1>
+				<h1 className="text-white text-5xl font-grotesk">Innovate.</h1>
+				<h1 className="text-white text-5xl font-grotesk">Create.</h1>
+			</div>
+			<div className="h-[60vh] flex justify-end items-center">
+				<div className="h-[260px] shadow-xl shadow-blue-600  rounded-xl overflow-hidden">
+					<img
+						src="https://www.shutterstock.com/image-vector/futuristic-robot-interacting-digital-data-260nw-2500204599.jpg"
+						alt=""
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default Filler
+export default Filler;
--- src/pages/Home/Innovations.jsx
+++ src/pages/Home/Innovations.jsx
@@ -55,7 +55,7 @@ function Innovations() {
   return (
     <section className="py-12 bg-accent3">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h2 className="text-3xl font-extrabold text-gray-900 mb-8 text-center">Innovations</h2>
+        <h2 className="text-4xl font-extrabold text-white font-grotesk mb-8 text-center">Innovations</h2>
         <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {innovations.map(({ id, title, description, image }) => (
             <div
