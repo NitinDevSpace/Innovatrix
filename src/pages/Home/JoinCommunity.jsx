1f9974822c6b768b61cf8ccfddaa4e61501c1031
Nitin <nitindevspace@gmail.com>
2025-10-15 14:48:39 +0530

    Community images

--- src/pages/Home/JoinCommunity.jsx
+++ src/pages/Home/JoinCommunity.jsx
@@ -1,36 +1,44 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function JoinCommunity() {
+	const naviagte = useNavigate();
+
 	const features = [
 		{
 			title: "Networking Opportunities",
 			description:
 				"Connect with like-minded innovators and expand your professional network.",
-			image: "https://image-placeholder.com/images/actual-size/320x200.png",
+			image:
+				"https://cmscritic.com/ms-content/uploads/2023/08/top-social-networking-sites_wmysmq.jpg?format=auto&width=2048",
 		},
 		{
 			title: "Innovative Ideas",
 			description:
 				"Be part of a community that fosters creativity and groundbreaking ideas.",
-			image: "https://image-placeholder.com/images/actual-size/320x200.png",
+			image:
+				"https://www.teachingtimes.com/wp-content/uploads/2018/01/Innovation2-scaled.jpg",
 		},
 		{
 			title: "Learning Resources",
 			description:
 				"Access exclusive tutorials, webinars, and workshops to enhance your skills.",
-			image: "https://image-placeholder.com/images/actual-size/320x200.png",
+			image:
+				"https://img.freepik.com/free-vector/education-infographic-template_1212-123.jpg?semt=ais_hybrid&w=740&q=80",
 		},
 		{
 			title: "Collaborative Projects",
 			description:
 				"Work together on exciting projects and bring your ideas to life.",
-			image: "https://image-placeholder.com/images/actual-size/320x200.png",
+			image:
+				"https://a.storyblok.com/f/99519/1100x619/66d49daabb/the-ultimate-guide-of-project-collaboration-blog__1_.png/m/2160x1216/filters:format(png)",
 		},
 		{
 			title: "Mentorship Programs",
 			description:
 				"Get guidance from industry experts to accelerate your growth.",
-			image: "https://image-placeholder.com/images/actual-size/320x200.png",
+			image:
+				"https://www.aihr.com/wp-content/uploads/mentorship-programs-cover.png",
 		},
 	];
 
@@ -54,7 +62,7 @@ function JoinCommunity() {
 						<img
 							src={image}
 							alt={title}
-							className="w-32 h-32 object-cover rounded-md mb-4"
+							className="w-32 h-32 object-cover rounded-xl mb-4"
 						/>
 						<h3 className="text-xl font-semibold mb-2 text-center">{title}</h3>
 						<p className="text-center text-sm">{description}</p>
@@ -62,7 +70,12 @@ function JoinCommunity() {
 				))}
 			</div>
 			<div className="flex justify-center items-center mt-12">
-				<button className="bg-white text-primary font-semibold px-6 py-3 rounded-md shadow-md hover:bg-gray-100 transition-colors duration-300">
+				<button
+					onClick={() => {
+						naviagte("/builders");
+					}}
+					className="bg-white text-primary font-semibold px-6 py-3 rounded-md shadow-md hover:scale-105 hover:bg-accent3 transition-colors duration-300"
+				>
 					Join Community
 				</button>
 			</div>
